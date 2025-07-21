f36fd307768982ade45d5226324530325392a0b0
Kazuma Ito <zarigani2x@gmail.com>
2025-07-22 02:33:57 +0900

    refactor: enhance layout and styling of PackageJsonCard component

--- src/components/pacage-json-card.tsx
+++ src/components/pacage-json-card.tsx
@@ -9,49 +9,63 @@ type Props = {
 
 const PackageJsonCard = ({ data }: Props) => {
   return (
-    <div className="border p-4">
-      <div className="flex items-center">
-        <Link href={data.url} className="font-medium">
-          {data.name}
-        </Link>
-        <div className="text-sm text-foreground/60 ml-3" title="Version">
-          {data.version}
-        </div>
-        <div className="flex ml-3">
-          {data.license && (
-            <Badge variant="secondary" title="License">
-              <ScaleIcon />
-              {data.license}
-            </Badge>
-          )}
-        </div>
-      </div>
-      <div className="flex flex-col gap-4 sm:flex-row mt-4">
-        <div className="flex-1">
-          <div className="text-sm">
-            {data.description || "No description available"}
+    <div className="p-4 flex flex-col gap-4 md:flex-row border">
+      <div className="flex-1">
+        <div className="flex items-center">
+          <Link href={data.url} className="font-semibold">
+            {data.name}
+          </Link>
+          <div className="text-sm text-foreground/60 ml-3" title="Version">
+            {data.version}
+          </div>
+          <div className="flex ml-3">
+            {data.license && (
+              <Badge variant="secondary" title="License">
+                <ScaleIcon />
+                {data.license}
+              </Badge>
+            )}
           </div>
         </div>
-        <div className="flex-1">
-          {data.scripts && (
-            <div>
-              <div className="text-sm">Scripts</div>
-              <div className="flex flex-wrap gap-2 mt-2">
-                {Object.entries(data.scripts).map(
-                  ([scriptName, scriptCommand]) => (
-                    <Badge
-                      variant="outline"
-                      key={scriptName}
-                      className="text-sm font-medium"
-                    >
-                      {scriptName}
-                    </Badge>
-                  )
-                )}
-              </div>
-            </div>
-          )}
+        <div className="text-sm mt-2">
+          {data.description || "No description available"}
         </div>
+      </div>
+      <div className="flex-1 space-y-4">
+        {data.keywords && (
+          <div>
+            <div className="text-sm">Keywords</div>
+            <div className="flex flex-wrap gap-2 mt-2">
+              {data.keywords.map((keyword) => (
+                <Badge
+                  variant="outline"
+                  key={keyword}
+                  className="text-sm font-medium"
+                >
+                  {keyword}
+                </Badge>
+              ))}
+            </div>
+          </div>
+        )}
+        {data.scripts && (
+          <div>
+            <div className="text-sm">Scripts</div>
+            <div className="flex flex-wrap gap-2 mt-2">
+              {Object.entries(data.scripts).map(
+                ([scriptName, scriptCommand]) => (
+                  <Badge
+                    variant="outline"
+                    key={scriptName}
+                    className="text-sm font-medium"
+                  >
+                    {scriptName}
+                  </Badge>
+                )
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
