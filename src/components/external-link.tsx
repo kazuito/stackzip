21692ede0c943fed6ea8eb6b64a82d52580a224b
Kazuma Ito <zarigani2x@gmail.com>
2025-07-22 08:30:31 +0900

    fix: improve styling and animations for external link component

--- src/components/external-link.tsx
+++ src/components/external-link.tsx
@@ -12,11 +12,11 @@ const ExternalLink = ({ href, className, ...props }: Props) => {
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className={cn("flex text-sm items-end gap-0.5 hover:text-foreground", className)}
+      className={cn("flex text-sm items-end gap-0.25 hover:text-foreground group/link", className)}
       {...props}
     >
       <span>{props.children}</span>
-      <ArrowUpRightIcon className="size-4" strokeWidth={1.6} />
+      <ArrowUpRightIcon className="size-4 transition-transform group-hover/link:-translate-y-0.5 group-hover/link:translate-x-0.5" strokeWidth={1.6} />
     </Link>
   );
 };
