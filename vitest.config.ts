87429046e2361a806966ec067e74cf5d0d324126
Kazuma Ito <zarigani2x@gmail.com>
2025-07-22 09:36:53 +0900

    load env on test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vitest/config";
 import { resolve } from "path";
+import { loadEnv } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   test: {
     environment: "jsdom",
+    env: loadEnv(mode, process.cwd(), ""),
   },
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
     },
   },
-});
+}));
