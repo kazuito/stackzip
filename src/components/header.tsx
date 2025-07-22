1759f210a7f04bb085b8563780a41a4225e2f0b2
Kazuma Ito <zarigani2x@gmail.com>
2025-07-22 11:33:18 +0900

    add github link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../../public/logo.svg";
+import ExternalLink from "./external-link";
 
 const Header = () => {
   return (
-    <header className="mx-auto flex h-14 max-w-5xl items-center px-6">
+    <header className="mx-auto flex h-14 max-w-5xl items-center px-6 font-mono">
       <Link href="/" className="flex items-center">
         <Image src={logo} alt="Logo" width={32} height={32} className="mr-2 inline-block size-5" />
-        <div className="font-mono font-semibold">Stackzip</div>
+        <div className="font-semibold">Stackzip</div>
       </Link>
+      <div className="ml-auto">
+        <ExternalLink className="text-foreground/60" href="https://github.com/kazuito/stackzip">Source</ExternalLink>
+      </div>
     </header>
   );
 };
