257ef509fcef4dca71e59e12fd3f986b9102ec23
Kazuma Ito <zarigani2x@gmail.com>
2025-07-22 11:25:48 +0900

    add logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import Image from "next/image";
+import logo from "../../public/logo.svg";
 
 const Header = () => {
   return (
     <header className="mx-auto flex h-14 max-w-5xl items-center px-6">
-      <Link href="/" className="font-mono font-bold">
-        Stackzip
+      <Link href="/" className="flex items-center">
+        <Image src={logo} alt="Logo" width={32} height={32} className="mr-2 inline-block size-5" />
+        <div className="font-mono font-semibold">Stackzip</div>
       </Link>
     </header>
   );
