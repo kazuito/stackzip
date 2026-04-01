1f6b1823237af92b53bd2009241f508ea15c17af
Kazuma Ito <zarigani2x@gmail.com>
2026-04-01 20:03:36 +0900

    feat: initial commit

--- src/lib/env.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-const SecretEnvSchema = z.object({
-  GITHUB_TOKEN: z.string().min(1, "GITHUB_TOKEN is required"),
-});
-
-export const env = {
-  secret: SecretEnvSchema.parse(process.env),
-};
